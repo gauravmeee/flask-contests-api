8ad71dec58178868d287bef5d25d4eb17aeedbb9
Gaurav <gaurav28.official@gmail.com>
2025-02-13 04:11:24 +0530

    cors added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
+from flask_cors import CORS  # Import CORS
 import scrap
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 # Define API resource for fetching contest data
